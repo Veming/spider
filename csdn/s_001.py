722db4971f74f833a98eb2f0faa81078703da10d
Veming <34410996+Veming@users.noreply.github.com>
2018-05-22 18:13:16 +0800

    Update s_001.py

--- csdn/s_001.py
+++ csdn/s_001.py
@@ -3,15 +3,32 @@
 # create by :苏杭
 # util pakage: request 
 from bs4 import BeautifulSoup
+from redis import StrictRedis
 import re
 import requests
+
+redis = StrictRedis(host='localhost', port=6379, db=0)
 
 __name__ == '__main__'
 
 
 def getHTMLText(url):
     try:  
-        r = requests.get(url, timeout=30)  
+        headers = {
+            'Host': 'blog.csdn.net',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://blog.csdn.net/nav/cloud',
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-Tingyun-Id': 'wl4EtIR_7Is;r=976669768',
+            'Cookie': 'uuid_tt_dd=4099473511411770564_20180522; dc_session_id=10_1526976401745.516297; TY_SESSION_ID=44a0e163-9d87-49c1-8a47-8cb70dbb5695; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1526976641; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1526976651; ADHOC_MEMBERSHIP_CLIENT_ID1.0=80021b53-245d-2f0a-9e86-a5c831756bf7; dc_tos=p94eq2',
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+        }
+        r = requests.get(url, headers = headers, timeout=30)  
         r.raise_for_status()  
         r.encoding = r.apparent_encoding  
         return r.text  
@@ -57,6 +74,8 @@ def spider():
         time = re.findall(timeRule,html)
         if len(title) == 0 or len(time) == 0 :
             continue
+        data = "title : "+ title[0]+ "time : "+ time[0], "link : "+ link
+        redis.publish("redisChat",data)
         print("title : ", title[0], "time : ", time[0], "link : ", link)
 
 
@@ -67,3 +86,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
